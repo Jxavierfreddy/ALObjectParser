f01f7d4ae7340b024b5f5a27d9e0875e7fb4e1b9
Jxavierfreddy <jxavier@tegos.eu>
2025-06-11 10:51:46 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using ALObjectParser.Library;
+using System.Text.Json;
+
+string path = args.FirstOrDefault(a => a.StartsWith("--path="))?.Split("=")[1] ?? ".";
+string query = args.FirstOrDefault(a => a.StartsWith("--query="))?.Split("=")[1] ?? "";
+
+var objects = ALObjectReaderNew.ParseFolder(path);
+
+var results = objects
+    .SelectMany(obj => obj.Methods ?? new List<ALObjectParser.Library.Models.ALMethod>())
+    .Where(method => method.Name.Contains(query, StringComparison.OrdinalIgnoreCase))
+    .Select(m => new {
+        Object = m.Parent?.Name,
+        Method = m.Name,
+        m.Summary
+    });
+
+Console.WriteLine(JsonSerializer.Serialize(results));
